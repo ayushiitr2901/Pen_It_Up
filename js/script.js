acd209061b8cd791d0f9edc0cfacd9462f310219
ayushiitr2901 <ayush.shrivastava2901@gmail.com>
2020-10-12 22:33:57 +0530

    major css changes

---
+++ js/script.js
@@ -0,0 +1,11 @@
+VANTA.FOG({
+    el: "#header",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    baseColor: 0xffaaaa,
+    blurFactor: 0.49
+  });
+
